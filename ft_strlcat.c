27e56be21c49c0163691c6c342d039844606ef95
sdurr <sdurr@e2r12p15.42.fr>
2014-11-14 16:39:31 +0100

    modif

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 11:28:36 by sdurr             #+#    #+#             */
-/*   Updated: 2014/11/13 15:54:01 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/11/14 15:46:22 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,22 @@ size_t		ft_strlcat(char *dst, const char *src, size_t size)
 
 	tmp = size;
 	i = 0;
-	while (size != 0)
+	if (src)
 	{
-		if (src[i] != '\0')
-			dst[i] = src[i];
-		i++;
-		src++;
-		size--;
+		while (size > 1)
+		{
+			dst[i] = *src;
+			i++;
+			size--;
+		}
+		dst[i] = '\0';
 	}
-	dst[i] = '\0';
-	return (ft_strlen(dst) + ft_strlen (src) + tmp);
+	if (tmp == 1 || tmp == 0)
+		return (ft_strlen(src));
+	if (*src == '\0')
+		return (0);
+	if (ft_strlen(src) <= tmp)
+		return (ft_strlen(dst) + ft_strlen (src) - 1);
+	else
+		return (ft_strlen (src) + tmp - 2);
 }
