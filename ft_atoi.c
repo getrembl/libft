3ab161c1f245739747bc7ed4497328b8089487a3
sdurr <sdurr@e3r1p12.42.fr>
2014-11-18 17:39:30 +0100

    test atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 15:43:09 by sdurr             #+#    #+#             */
-/*   Updated: 2014/11/18 17:19:33 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/11/18 17:37:21 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,18 @@ int		ft_atoi(const char *str)
 	{
 		while (str[i] >= 1 && str[i] <= 32)
 			i++;
+		if (ft_strlen(str) > 12)
+			return (0);
 		if ((str[i] == '-') || (str[i] == '+'))
 		{
 			if (str[i] == '-')
 				neg = -1;
+			else
+				neg = 1;
 			i++;
 		}
 		while (str[i] >= '0' && str[i] <= '9')
 			nb = (nb * 10) + (str[i++] - '0');
-		if (neg == -1)
 			nb = nb * neg;
 	}
 	return (nb);
