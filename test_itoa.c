8045cda1931782d1a47054b5fd71fc9bf7452a16
sdurr <sdurr@e3r1p12.42.fr>
2014-11-18 19:07:35 +0100

    test itoa non ok +2147483648

--- test_itoa.c
+++ test_itoa.c
@@ -1,40 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   test_itoa.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/12 17:11:27 by sdurr             #+#    #+#             */
-/*   Updated: 2014/11/18 19:06:40 by sdurr            ###   ########.fr       */
+/*   Created: 2014/11/18 18:41:53 by sdurr             #+#    #+#             */
+/*   Updated: 2014/11/18 19:06:55 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "libft.h"
 
-char	*ft_itoa(int n)
+int main()
 {
-	char	*ret;
-	int		i;
-
-	i = 0;
-	if (!(ret = (char *)malloc(sizeof(char) * ft_long_int(n) + 1)))
-		return (NULL);
-	if (n == -2147483648)
-		return (ft_strdup("-2147483648"));
-	if (n < 0)
-	{
-		ret[i++] = '-';
-		n = n * -1;
-	}
-	while (n >= 10 && n <= 2147483647)
-	{
-		ret[i++] = n % 10 + '0';
-		n = n / 10;
-	}
-	if (n < 10)
-		ret[i++] = n + '0';
-	ret[i] = '\0';
-	ret = ft_revers(ret);
-	return (ret);
+			printf("%s\n", ft_itoa(-2147483648));
+			printf("%s\n", ft_itoa(2147483648));
+			printf("%s\n", ft_itoa(-218));
+			printf("%s\n", ft_itoa(218));
+			printf("%s\n", ft_itoa(+218));
+			printf("%s\n", ft_itoa(+2147483648));
+   return (0);
 }
